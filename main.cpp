ba8e3386acd989a30cac1ecfc5eeaea3d9a5219f
안장훈 <wkdgns135@gmail.com>
2024-12-30 12:40:34 +0900

    테스트 코드 추가

--- main.cpp
+++ main.cpp
@@ -73,33 +73,51 @@ public:
 };
 
 int main() {
-	SimpleVector<int> sv(5);
+    // 기본 생성자 테스트
+    SimpleVector<int> sv1;
+    cout << "기본 생성자 테스트: capacity = " << sv1.capacity() << ", size = " << sv1.size() << endl;
 
-	sv.push_back(3);
-	sv.push_back(1);
-	sv.push_back(4);
-	sv.push_back(1);
-	sv.push_back(5);
-	sv.push_back(10);
+    // 크기를 지정하는 생성자 테스트
+    SimpleVector<int> sv2(5);
+    cout << "크기를 지정하는 생성자 테스트: capacity = " << sv2.capacity() << ", size = " << sv2.size() << endl;
 
-	for (int i = 0; i < sv.size(); i++) {
-		cout << sv[i] << " ";
-	}
-	cout << '\n';
+    // push_back 테스트
+    sv2.push_back(3);
+    sv2.push_back(1);
+    sv2.push_back(4);
+    sv2.push_back(1);
+    sv2.push_back(5);
+    sv2.push_back(10); // 크기 초과로 배열 확장
+    cout << "데이터 추가 후: ";
+    for (int i = 0; i < sv2.size(); i++) {
+        cout << sv2[i] << " ";
+    }
+    cout << "\ncapacity = " << sv2.capacity() << ", size = " << sv2.size() << endl;
 
-	sv.sortData();
+    // pop_back 테스트
+    sv2.pop_back();
+    sv2.pop_back();
+    cout << "데이터 제거 후: ";
+    for (int i = 0; i < sv2.size(); i++) {
+        cout << sv2[i] << " ";
+    }
+    cout << "\ncapacity = " << sv2.capacity() << ", size = " << sv2.size() << endl;
 
-	for (int i = 0; i < sv.size(); i++) {
-		cout << sv[i] << " ";
-	}
-	cout << '\n';
+    // sortData 테스트
+    sv2.sortData();
+    cout << "정렬 후 데이터: ";
+    for (int i = 0; i < sv2.size(); i++) {
+        cout << sv2[i] << " ";
+    }
+    cout << endl;
 
-	sv.pop_back();
-	sv.pop_back();
+    // 복사 생성자 테스트
+    SimpleVector<int> sv3 = sv2; // 복사 생성자 호출
+    cout << "복사된 데이터: ";
+    for (int i = 0; i < sv3.size(); i++) {
+        cout << sv3[i] << " ";
+    }
+    cout << "\ncapacity = " << sv3.capacity() << ", size = " << sv3.size() << endl;
 
-	for (int i = 0; i < sv.size(); i++) {
-		cout << sv[i] << " ";
-	}
-
-	cout << '\n';
+    return 0;
 }
